e7bbf3d987999d5842e3d22ecffb8a5c24d928c2
da.teng <da.teng@bytedance.com>
2024-05-16 00:29:29 -0700

    add images

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
-import pygame
 import sys
-pygame.init()
 
-screen = pygame.display.set_mode((1200, 800))
-screen.fill((60,80,90))
-pygame.display.set_caption("Alien Invasion")
+import pygame
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            print("Game Over")
-            sys.exit()
+from settings import Settings
 
-    pygame.display.flip()
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+
+    def __init__(self):
+        """Initialize the game, and create game resources."""
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion")
+
+    def run_game(self):
+        """Start the main loop for the game."""
+        while True:
+            # Watch for keyboard and mouse events.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
+
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+            self.clock.tick(60)
+
+
+if __name__ == '__main__':
+    # Make a game instance, and run the game.
+    ai = AlienInvasion()
+    ai.run_game()
