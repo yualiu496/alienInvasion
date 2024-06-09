eff403be12de81dc82ade11adb5d6de481d16221
yualiu496 <yualiu496@gmail.com>
2024-06-09 13:29:17 -0700

    color and picture

--- settings.py
+++ settings.py
@@ -6,14 +6,21 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (10,10,240)
+        self.bg_color = (230, 230, 230)
 
         # Ship settings
         self.ship_speed = 5
 
         # Bullet settings
-        self.bullet_speed = 6.0
+        self.bullet_speed = 10.0
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 30
+
+        self.alien_speed = 3.0
+        self.fleet_drop_speed = 40
+
+         # fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
+        self.ship_limit = 0
